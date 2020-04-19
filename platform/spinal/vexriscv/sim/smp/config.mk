ad1aa82fdd7e5b77ed790aca8dd7dcd0f8f84c8e
Dolu1990 <charles.papon.90@gmail.com>
2020-04-19 12:57:26 +0200

    update plateform vexriscv

--- platform/spinal/vexriscv/sim/smp/config.mk
+++ platform/spinal/vexriscv/sim/smp/config.mk
@@ -25,20 +25,8 @@ PLATFORM_RISCV_CODE_MODEL = medany
 FW_TEXT_START=0x80000000
 FW_DYNAMIC=y
 FW_JUMP=y
-ifeq ($(PLATFORM_RISCV_XLEN), 32)
-  # This needs to be 4MB aligned for 32-bit system
-  FW_JUMP_ADDR=0x80400000
-else
-  # This needs to be 2MB aligned for 64-bit system
-  FW_JUMP_ADDR=0x80200000
-endif
-FW_JUMP_FDT_ADDR=0x82200000
+FW_JUMP_ADDR=0xC0000000
+FW_JUMP_FDT_ADDR=0xC1000000
 FW_PAYLOAD=y
-ifeq ($(PLATFORM_RISCV_XLEN), 32)
-  # This needs to be 4MB aligned for 32-bit system
-  FW_PAYLOAD_OFFSET=0x400000
-else
-  # This needs to be 2MB aligned for 64-bit system
-  FW_PAYLOAD_OFFSET=0x200000
-endif
-FW_PAYLOAD_FDT_ADDR=0x82200000
+FW_PAYLOAD_OFFSET=0x400000
+FW_PAYLOAD_FDT_ADDR=0xC1000000
